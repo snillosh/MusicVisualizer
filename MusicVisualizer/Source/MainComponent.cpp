210e8228a6039cae748afb5873b71d23f5dce71b
snillosh <69086485+snillosh@users.noreply.github.com>
2021-11-23 15:02:30 +0000

    Audio playback setup

--- MusicVisualizer/Source/MainComponent.cpp
+++ MusicVisualizer/Source/MainComponent.cpp
@@ -1,11 +1,31 @@
 #include "MainComponent.h"
 
 //==============================================================================
-MainComponent::MainComponent()
+MainComponent::MainComponent() : forwardFFT(fftOrder),
+                                 window(fftSize,
+                                 juce::dsp::WindowingFunction<float>::blackman),
+                                 state(Stopped),
+                                 openButton("Open"),
+                                 playButton("Play"),
+                                 stopButton("Stop")
 {
     // Make sure you set the size of the component after
     // you add any child components.
     setSize (1280, 1280);
+    setAudioChannels(0, 2);
+    Timer::startTimerHz(60);
+    
+    openButton.onClick = [this] { openButtonClicked(); };
+    addAndMakeVisible(&openButton);
+    
+    playButton.onClick = [this] { playButtonClicked();};
+    addAndMakeVisible(&playButton);
+    
+    stopButton.onClick = [this] { stopButtonClicked(); };
+    addAndMakeVisible(&stopButton);
+    
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener(this);
 
     // Some platforms require permissions to open input channels so request that here
     if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
@@ -17,59 +37,155 @@ MainComponent::MainComponent()
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels (0, 2);
     }
 }
 
 MainComponent::~MainComponent()
 {
     // This shuts down the audio device and clears the audio source.
+    stopTimer();
     shutdownAudio();
 }
 
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    
+    
+    
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    if (readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    
+    transportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
+    transportSource.releaseResources();
 }
 
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (Colours::black);
 
-    // You can add your drawing code here!
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    openButton.setBounds(10, 10, getWidth() - 20, 30);
+    playButton.setBounds(10, 50, getWidth() - 20, 30);
+    stopButton.setBounds(10, 90, getWidth() - 20, 30);
+}
+
+void MainComponent::timerCallback()
+{
+    
+}
+
+void MainComponent::changeListenerCallback (juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+            changeState(Playing);
+        else if ((state == Stopping) || (state == Playing))
+            changeState(Stopped);
+        else if (Pausing == state)
+            changeState(Paused);
+    }
+}
+
+void MainComponent::openButtonClicked()
+{
+    chooser = std::make_unique<juce::FileChooser> ("Select a Wave or MP3 file to play...",
+                                                   juce::File{},
+                                                   "*.wav;*.mp3");
+    auto chooserFlags = juce::FileBrowserComponent::openMode
+                      | juce::FileBrowserComponent::canSelectFiles;
+
+    chooser->launchAsync (chooserFlags, [this] (const juce::FileChooser& fc)
+    {
+        auto file = fc.getResult();
+
+        if (file != juce::File{})
+        {
+            auto* reader = formatManager.createReaderFor (file);
+
+            if (reader != nullptr)
+            {
+                auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);
+                transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);
+                playButton.setEnabled (true);
+                readerSource.reset (newSource.release());
+            }
+        }
+    });
+}
+
+void MainComponent::playButtonClicked()
+{
+    if ((state == Stopped) || (state == Paused)) {
+        changeState (Starting);
+    }
+    else if (state == Playing)
+        changeState(Pausing);
+    
+}
+
+void MainComponent::stopButtonClicked()
+{
+    if (state == Paused) {
+        changeState (Stopped);
+    }
+    else
+        changeState(Stopping);
+    
+}
+
+void MainComponent::changeState (TransportState newState)
+{
+    if (state != newState)
+    {
+        state = newState;
+        
+        switch (state) {
+            case Stopped:
+                playButton.setButtonText("Play");
+                stopButton.setButtonText("Stop");
+                stopButton.setEnabled(false);
+                transportSource.setPosition(0.0);
+                break;
+                
+            case Starting:
+                transportSource.start();
+                break;
+                
+            case Playing:
+                playButton.setButtonText("Pause");
+                stopButton.setButtonText("Stop");
+                stopButton.setEnabled(true);
+                break;
+                
+            case Pausing:
+                transportSource.stop();
+                
+            case Paused:
+                playButton.setButtonText("Resume");
+                stopButton.setButtonText("Return to Zero");
+                
+            case Stopping:
+                transportSource.stop();
+                break;
+        }
+    }
 }
--- MusicVisualizer/Source/MainComponent.h
+++ MusicVisualizer/Source/MainComponent.h
@@ -2,12 +2,21 @@
 
 #include <JuceHeader.h>
 
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+
+#include"ParticleComponent.h"
+
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent,
+                       public juce::ChangeListener,
+                       public juce::Timer
 {
 public:
     //==============================================================================
@@ -22,10 +31,66 @@ public:
     //==============================================================================
     void paint (juce::Graphics& g) override;
     void resized() override;
+    
+    void pushNextSampleIntoFifo (float sample) noexcept;
+    void drawNextFrameOfSpectrum();
+    
+    enum
+    {
+        fftOrder  = 11,
+        fftSize = 1 << fftOrder,
+        scopeSize = 512
+    };
 
 private:
     //==============================================================================
     // Your private member variables go here...
+    
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Pausing,
+        Paused,
+        Stopping
+    };
+    
+    void changeListenerCallback (juce::ChangeBroadcaster* source) override;
+    void changeState (TransportState newState);
+    
+    void timerCallback() override;
+    void drawFrame (juce::Graphics& g);
+    
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
+    
+    std::unique_ptr<juce::FileChooser> chooser;
+    
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    TransportState state;
+    
+    juce::TextButton openButton;
+    juce::TextButton playButton;
+    juce::TextButton stopButton;
+    
+    juce::dsp::FFT forwardFFT;
+    juce::dsp::WindowingFunction<float> window;
+    
+    float fifo[fftSize];
+    float fftData[2 * fftSize];
+    int fifoIndex = 0;
+    bool nextFFTBlockReady = false;
+    float scopeData [scopeSize];
+    
+    Point<float> centrePoint;
+    OwnedArray<ParticleComponent> particlesTopLeft;
+    OwnedArray<ParticleComponent> particlesTopRight;
+    OwnedArray<ParticleComponent> particlesBottomLeft;
+    OwnedArray<ParticleComponent> particlesBottomRight;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
